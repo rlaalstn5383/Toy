da188d1ea8b751648fdaf6c809150a0d1bdbfb0f
Minsoo Kim <mskim5383@gmail.com>
2016-11-28 15:08:35 +0900

    reg-macro: update function

--- reg-macro.js
+++ reg-macro.js
@@ -1,7 +1,7 @@
 // USAGE
 // c = register(과목이름, 교수님) -> 교수님 안 넣어도 됨
 // 콘솔에 정보 출력
-// 실행을 원할 경우->  c()
+// 실행을 원할 경우->  c(과목 인덱스)
 // 다시 검색을 하고 싶을 경우-> c = register(...)
 
 function register(courseName, prof) {
@@ -19,8 +19,28 @@ function register(courseName, prof) {
     }
     var nameSel = "tr:contains('" + courseName + "')";
     var profSel = ":contains('" + prof + "')";
-    var a = $("table#listTable").find(nameSel).filter(profSel).find("a");
+    var tr = $("table#listTable").find(nameSel).filter(profSel);
+    var a = tr.find("a[name = 'saveCourse']");
 
+    console.log("##########################");
     console.log("found " + a.length + " class");
-    return function() { setInterval(function() { a.click(); }, 1000); };
+    $.each(tr, function(index, value) {
+        console.log("# " + index + ": " $(value).children().eq(5).text());
+    })
+
+    console.log("call the return function with index");
+
+    function ret(index) {
+        $.each(tr, function(i, value) {
+            if (index == i) {
+                var a = value.find("a[name = 'saveCourse']");
+                setInterval(function() { a.click();}, 1000);
+                console.log("macro works!");
+                console.log("# " + $(value).children().eq(6).text() + " " + $(value).children().eq(5).text());
+                return;
+            }
+        });
+        console.log("incorrect index: " + index);
+    }
+    return ret;
 }
